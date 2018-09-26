da152ea44428443fed811064b5e8a7e82d2cb54c
admin <admin@ADMIN-LIU>
2018-09-26 15:20:32 +0800

    patien class

--- OdbMySQLPatient/OdbMySQLPatient/Patient.h
+++ OdbMySQLPatient/OdbMySQLPatient/Patient.h
@@ -1,1 +1,24 @@
 #pragma once
+#include<string>
+class Patient
+{
+public:
+	Patient();
+
+	const std::string& email() const;
+	void email(const std::string&);
+
+	const std::string& get_name() const;
+	std::string& set_name();
+
+	unsigned short getAge() const;
+	void setAge(unsigned short);
+
+private:
+	std::string email_;
+	std::string name_;
+	unsigned short age_;
+};
+
+#pragma db object(person)
+#pragma db member(person::email_) id
