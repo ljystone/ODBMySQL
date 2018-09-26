68babfc52ed505797c006d5f9761d3505ec9fe21
admin <admin@ADMIN-LIU>
2018-09-26 15:35:32 +0800

    pateien

--- OdbMySQLPatient/OdbMySQLPatient/Patient.h
+++ OdbMySQLPatient/OdbMySQLPatient/Patient.h
@@ -1,24 +1,39 @@
 #pragma once
 #include<string>
+
 class Patient
 {
 public:
 	Patient();
-
-	const std::string& email() const;
-	void email(const std::string&);
-
-	const std::string& get_name() const;
+	unsigned short getId() const; //id 
+	void setId(unsigned short);
+	const std::string& get_name() const; //name
 	std::string& set_name();
-
+	char getSex() const;
+	void setSex(char);
 	unsigned short getAge() const;
 	void setAge(unsigned short);
-
+	unsigned short getHeight() const;
+	void setHeight(unsigned short);
+	unsigned short getWeight() const;
+	void setWeight(unsigned short);
+	std::string getPatientHistory() const;
+	void setPatientHistory(std::string);
+	unsigned short getNeckLength_() const;
+	void setNeckLength_(unsigned short);
+	unsigned short getNeckWaistLength() const;
+	void setNeckWaistLength(unsigned short);
 private:
-	std::string email_;
+	unsigned short id_;
 	std::string name_;
+	char sex_;
 	unsigned short age_;
+	unsigned short height_;
+	unsigned short weight_;
+	std::string patientHistory_;
+	unsigned short neckLength_;
+	unsigned short neckWaistLength_;
 };
 
-#pragma db object(person)
-#pragma db member(person::email_) id
+#pragma db object(Patient)
+#pragma db member(Patient::email_) id
